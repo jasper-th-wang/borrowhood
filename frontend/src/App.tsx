51761be7e4cf8103ddc3189426c87ab962a2871f
Jasper Wang <jasper8777@icloud.com>
2025-01-18 13:21:26 -0800

    setup react query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
 import '@mantine/core/styles.css';
 
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
 
 export default function App() {
+  const queryClient = new QueryClient()
   return (
-    <MantineProvider theme={theme}>
-      <Router />
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider theme={theme}>
+        <Router />
+      </MantineProvider>
+    </QueryClientProvider>
   );
 }
