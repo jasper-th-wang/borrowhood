ea7f2e483c21e87131cc37a1fc829a9125e88b7b
Gursewak Singh <gursewaktut@gmail.com>
2025-01-18 16:40:18 -0800

    completed google vision api

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,61 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from google.cloud import vision
+from llamaapi import LlamaAPI
+import os
+import json
+
+print(os.environ['LLAMA_API_KEY'])
+LLAMA_API_KEY = os.environ['LLAMA_API_KEY']
 
 app = FastAPI()
-
+llama = LlamaAPI(LLAMA_API_KEY)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+def encode_image_to_base64(image):
+    """Convert an image file to base64 string."""
+    return base64.b64encode(image.read_bytes()).decode('utf-8')
+
+@app.post("/image/annotate")
+async def annotate_image(image: UploadFile = File(...)):
+    image_bytes = await image.read()
+
+    #with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as tmp_file:
+        #tmp_file.write(image_bytes)
+        #tmp_file_path = tmp_file.name
+
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
+
+    # The URI of the image file to annotate
+    #file_uri = "https://upload.wikimedia.org/wikipedia/en/thumb/6/62/BraveNewWorld_FirstEdition.jpg/220px-BraveNewWorld_FirstEdition.jpg"
+
+    image = vision.Image(content=image_bytes)
+    #image.source.image_uri = file_uri
+
+    # Performs label detection on the image file
+    #response = client.label_detection(image=tmp_file)
+    response = client.text_detection(image=image)
+    response2 = client.label_detection(image=image)
+    texts = response.text_annotations
+    labels = response2.label_annotations
+    
+    print("Texts:")
+    for text in texts:
+        print(text.description)
+
+    print("Labels:")
+    for label in labels:
+        print(label.description)
+
+        
+    base64_image = encode_image_to_base64(image)
+    #return labels
+
+    
+    return {"message": "In the annotating function"}
+
+if __name__ == '__main__':
+    uvicorn.run(app, loop='asyncio')
---
+++ frontend/src/pages/Listing.page.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+export function ListingPage() {
+
+  const [imageData, setImageData] = useState(false);
+
+  const handleChange = (e) => {
+    setImageData(e.target.files[0]);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // using FormData to send the file in multipart/form-data format.
+    const formData = new FormData();
+    formData.append("image", imageData);
+    formData.set("image_data", imageData);
+
+    const res = await fetch("http://localhost:8000/image/annotate", {
+      method: "POST",
+      body: formData,
+    });
+
+    if (res.ok) {
+      res.json().then((data) => console.log(data));
+    } else {
+      console.log("error");
+    }
+    console.log(formData.get("imageData"));
+  };
+
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="file" accept="image/jpg" onChange={handleChange} />
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
+          Upload
+        </button>
+      </form>
+    </>
+  );
+}
+
