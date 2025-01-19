c3cbc308238e467f48f0f137f0e5d98461024188
Koki-Kazaore <kazaore@icloud.com>
2025-01-19 01:09:02 -0800

    add page to add item

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
 import { TestingPage } from '@/pages/Testing.page';
+import { AddItemPage } from './pages/AddItem.page';
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
   {
     path: '/testing',
     element: <TestingPage />,
+  },
+  {
+    path: '/add-item',
+    element: <AddItemPage />,
   },
 ]);
 
