ddc5061eecc3d8906e339bb8e0b4fb72e1dfe5c1
Jasper Wang <jasper8777@icloud.com>
2025-01-29 20:15:21 -0800

    change express port

--- backend-rewrite/src/index.ts
+++ backend-rewrite/src/index.ts
@@ -1,6 +1,6 @@
 import app from './app';
 require('dotenv').config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   /* eslint-disable no-console */
   console.log(`Listening: http://localhost:${port}`);
